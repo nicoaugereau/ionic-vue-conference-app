46170b737a786cff722246bd452c90569246a243
nicoaugereau <32208373+nicoaugereau@users.noreply.github.com>
2022-09-03 12:16:28 +0200

    cypress update

--- cypress.config.ts
+++ cypress.config.ts
@@ -46,7 +46,6 @@ export default defineConfig({
       return require("./cypress/plugins/index.js")(on, config);
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature,features}",
-    experimentalStudio: true,
   },
   component: {
     devServer: {
